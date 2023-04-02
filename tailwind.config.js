5c321c4bc8d30363e449b3f360b41ad99b8949ac
rodrtigo.calle <rodrigoc_0@hotmail.com>
2023-04-01 20:59:44 -0500

    fix: adding taildwind configs and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -9,7 +8,13 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      'main-color': '#114C85',
+      'secondary-color': '#3DC7FF',
+      
+      'gray-input-color': '#E0E0E0',
+      'border-gray-color': '#888888'
+    },
   },
   plugins: [],
 }
